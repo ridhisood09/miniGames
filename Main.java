45ebcebf809a2bb4e21817cef22e4597eae9003b
ridhisood09 <80955404+ridhisood09@users.noreply.github.com>
2023-07-21 13:06:30 +0530

    Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+// Game - Noughts and Crosses using simple recursion and backtracking
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
